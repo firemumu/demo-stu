2519d61c537a15dcb34c2667333a8ff9a9de8dc4
moci <firemumu@qq.com>
2018-09-12 10:11:49 +0800

    学习项目，IDEA整合

---
+++ src/main/java/cn/excgo/demostu/DemoStuApplication.java
@@ -0,0 +1,12 @@
+package cn.excgo.demostu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStuApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/excgo/demostu/DemoStuApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.excgo.demostu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoStuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
