a80d528946838949e352767238435b983a90248f
moci <firemumu@qq.com>
2018-09-17 10:24:27 +0800

    整合JDBC

---
+++ src/main/java/cn/excgo/demostu/jdbc/UserController.java
@@ -0,0 +1,30 @@
+package cn.excgo.demostu.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public String index() {
+        return "user";
+    }
+
+    @GetMapping("/all")
+    public List<User> queryUsers() {
+        // 查询所有用户
+        String sql = "select * from t_user";
+        return jdbcTemplate.query(sql, new Object[]{}, new BeanPropertyRowMapper<>(User.class));
+    }
+
+}
